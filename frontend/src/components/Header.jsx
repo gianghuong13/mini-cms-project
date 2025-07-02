271ffda7e13ef66baaa83e56f67c3d9acb478e7c
gianghuong13 <nhg130904@gmail.com>
2025-07-02 16:15:00 +0700

    feat: store theme in localStorage

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,18 @@
-const Header = () => {
+const Header = ({ onToggleItem, theme }) => {
   return (
     <header className='bg-gray-200 shadow px-6 py-5 flex justify-between items-center'>
         <h1 className='text-2xl font-semibold'>Welcome to CMS</h1>
-        <div>Tài khoản</div>
+        <div className="flex">
+          <div>Tài khoản</div>
+          <div>
+            <button
+              onClick={onToggleItem}
+              className="bg-blue-500 text-white px-3 py-1 mx-5 rounded"
+            >
+              {theme === "light" ? "Dark Mode" : "Light Mode"}
+            </button>
+          </div>
+        </div>
     </header>
   )
 }
--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -1,15 +1,33 @@
+import { useState, useEffect } from "react"
 import Header from "./Header"
 import Sidebar from "./Sidebar"
 import { Outlet } from "react-router-dom"
 
 const MainLayout = () => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect (() => {
+    const savedTheme = localStorage.getItem("theme");
+    const initialTheme = savedTheme || "light";
+    setTheme(initialTheme);
+    document.documentElement.className = initialTheme;
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+    document.documentElement.className = newTheme;
+  };
+
   return (
+
     <div className='h-screen flex flex-col px-3'>
-        <Header />
+        <Header onToggleItem={toggleTheme} theme={theme}/>
 
         <div className='flex flex-1 overflow-hidden'>
             <Sidebar />
-            <main className='flex-1 p-6 overflow-y-auto bg-white'>
+            <main className='flex-1 p-6 overflow-y-auto bg-white dark:bg-gray-900 dark:text-white'>
                 <Outlet />
             </main>
         </div>
